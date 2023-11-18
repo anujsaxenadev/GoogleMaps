ce9deace5943afc47d296a0feb507ab34636a3c5
anujsaxenadev <anuj.saxena.dev@gmail.com>
2023-11-18 18:27:29 +0530

    testing multiple threads

--- app/src/main/java/com/wordpress/anujsaxenadev/googlemaps/features/map/view/WebViewInterceptor.kt
+++ app/src/main/java/com/wordpress/anujsaxenadev/googlemaps/features/map/view/WebViewInterceptor.kt
@@ -1,10 +1,12 @@
 package com.wordpress.anujsaxenadev.googlemaps.features.map.view
 
+import android.util.Log
 import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.wordpress.anujsaxenadev.googlemaps.features.map.view_model.MapViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
 class WebViewInterceptor(
@@ -15,9 +17,13 @@ class WebViewInterceptor(
         request: WebResourceRequest?
     ): WebResourceResponse? {
         return runBlocking {
+            Log.e("anuj-log-called", request?.url.toString())
+            delay((1..10).random() * 1000L)
             mapViewModel.checkResourceAvailability(request).fold({
+                Log.e("anuj-log-resource-available", request?.url.toString())
                 it
             }, {
+                Log.e("anuj-log-resource-not-available", request?.url.toString())
                 super.shouldInterceptRequest(view, request)
             })
         }
