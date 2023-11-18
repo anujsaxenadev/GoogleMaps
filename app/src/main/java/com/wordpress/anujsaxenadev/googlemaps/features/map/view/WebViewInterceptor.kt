762bfaba3535a88e47fb7ae784fec554db0d5ec6
anujsaxenadev <anuj.saxena.dev@gmail.com>
2023-11-18 19:28:16 +0530

    concurrent api handling with async and ioscope

--- app/src/main/java/com/wordpress/anujsaxenadev/googlemaps/features/map/view/WebViewInterceptor.kt
+++ app/src/main/java/com/wordpress/anujsaxenadev/googlemaps/features/map/view/WebViewInterceptor.kt
@@ -1,25 +1,43 @@
 package com.wordpress.anujsaxenadev.googlemaps.features.map.view
 
+import android.util.Log
 import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.wordpress.anujsaxenadev.googlemaps.features.map.view_model.MapViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 class WebViewInterceptor(
     private val mapViewModel: MapViewModel
 ): WebViewClient() {
+    private val ioScope: CoroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
     override fun shouldInterceptRequest(
         view: WebView?,
         request: WebResourceRequest?
     ): WebResourceResponse? {
-        return runBlocking {
+        Log.e("anuj-log-called", request?.url.toString())
+        val result = ioScope.async{
+            Log.e("anuj-log-started", request?.url.toString())
+            delay((1..5000L).random())
             mapViewModel.checkResourceAvailability(request).fold({
+                Log.e("anuj-log-was-cached", request?.url.toString())
                 it
             }, {
+                Log.e("anuj-log-was-not-called", request?.url.toString())
                 super.shouldInterceptRequest(view, request)
             })
+        }
+        return runBlocking {
+            Log.e("anuj-log-awaited", request?.url.toString())
+            result.await()
         }
     }
 }
