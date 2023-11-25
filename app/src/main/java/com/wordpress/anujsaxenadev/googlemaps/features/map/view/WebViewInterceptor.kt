9fd0f040d57e9e50b71781685e4ac0688dd8d912
anujsaxenadev <anuj.saxena.dev@gmail.com>
2023-11-25 11:42:57 +0530

    Non Blocking Calls

--- app/src/main/java/com/wordpress/anujsaxenadev/googlemaps/features/map/view/WebViewInterceptor.kt
+++ app/src/main/java/com/wordpress/anujsaxenadev/googlemaps/features/map/view/WebViewInterceptor.kt
@@ -4,43 +4,73 @@ import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import com.wordpress.anujsaxenadev.googlemaps.core.extensions.getContentEncoding
+import com.wordpress.anujsaxenadev.googlemaps.core.extensions.getContentType
 import com.wordpress.anujsaxenadev.googlemaps.features.map.view_model.MapViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.consume
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
+import java.io.InputStream
+
 
 class WebViewInterceptor(
     private val mapViewModel: MapViewModel
 ): WebViewClient() {
-    private val ioScope: CoroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
     override fun shouldInterceptRequest(
         view: WebView?,
-        request: WebResourceRequest?
-    ): WebResourceResponse? {
-        val channel = Channel<WebResourceResponse?>()
-        // Launch a coroutine to handle the requests
-        ioScope.launch {
-//            Can try adding delay in the coroutine. You will see async calls happeing without blocking other requests.
-//            delay((0..3000L).random())
-            val result = mapViewModel.checkResourceAvailability(request)
-
-            result.fold({
-                channel.send(result.getOrNull())
-            },{
-                channel.send(super.shouldInterceptRequest(view, request))
-            })
-        }
-
-        // Receive the result from the channel
-        return runBlocking {
-            channel.consume {
-                receive()
-            }
-        }
+        request: WebResourceRequest
+    ): WebResourceResponse {
+		return WebResourceResponseWrapper(request, object: StreamInterruptionListener{
+			override suspend fun checkResourceAvailability(request: WebResourceRequest): Result<WebResourceResponse> {
+				return mapViewModel.checkResourceAvailability(request)
+			}
+		})
     }
+}
+
+interface StreamInterruptionListener{
+	suspend fun checkResourceAvailability(request: WebResourceRequest): Result<WebResourceResponse>
+}
+
+internal class WebResourceResponseWrapper(
+    private val request: WebResourceRequest,
+	private val streamListener: StreamInterruptionListener
+) :
+    WebResourceResponse(
+		request.getContentType(),
+		request.getContentEncoding(),
+		null
+	) {
+
+    override fun getData(): InputStream {
+        return InputStreamWrapper(request, streamListener)
+    }
+
+	internal class InputStreamWrapper(
+		private val request: WebResourceRequest,
+		private val streamListener: StreamInterruptionListener
+	) : InputStream() {
+		private var inputStream: InputStream? = null
+		private val ioScope: CoroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
+		override fun read(): Int {
+			return if(inputStream == null) {
+				val job = ioScope.async {
+					streamListener.checkResourceAvailability(request).getOrNull()
+				}
+
+				runBlocking {
+					inputStream = job.await()?.data
+				}
+				inputStream?.read()
+			}
+			else{
+				inputStream?.read()
+			} ?: 0
+		}
+	}
+
 }
